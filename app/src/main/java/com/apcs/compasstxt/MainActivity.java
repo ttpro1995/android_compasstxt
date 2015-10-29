052cfe3089a026c28af22c666ee52d7f63b8d864
thien.thai <tthien@apcs.vn>
2015-10-29 11:17:44 +0700

    show text on screen

--- app/src/main/java/com/apcs/compasstxt/MainActivity.java
+++ app/src/main/java/com/apcs/compasstxt/MainActivity.java
@@ -1,5 +1,8 @@
 package com.apcs.compasstxt;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,11 +11,19 @@ import android.os.AsyncTask;
 import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hahattpro.meowdebughelper.Mailer;
 import com.hahattpro.meowdebughelper.SaveFile;
@@ -32,10 +43,13 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
     private int LOGGING_MODE = 1;
     private int STOP_MODE = 0;
     private int MODE = STOP_MODE;
+    private File file = null;
 
     private LogCompass logCompass;
-    @Bind(R.id.mButton)
-    Button mButton;
+    @Bind(R.id.mButton) Button mButton;
+    @Bind(R.id.mSetting) Button mSetting;
+    @Bind(R.id.textView) TextView textView;
+    @Bind(R.id.scrollView) ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,27 +59,59 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         logCompass = new LogCompass();
         // initialize your android device sensor capabilities
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mButton.setBackgroundColor(getResources().getColor(R.color.white));
-
         //wake lock
         PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
         PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
                 "MyWakelockTag");
         wakeLock.acquire();
+        mButton.setBackgroundColor(getResources().getColor(R.color.light_blue));
+
+
     }
 
     @OnClick(R.id.mButton)
     void onClickStartLog(View view){
         if (MODE == STOP_MODE) {
             MODE = LOGGING_MODE;
+            logCompass = new LogCompass();
             logCompass.execute();
             mButton.setBackgroundColor(getResources().getColor(R.color.red));
 
         }
         else {
             MODE = STOP_MODE;
-            mButton.setBackgroundColor(getResources().getColor(R.color.white));
+
+            mButton.setBackgroundColor(getResources().getColor(R.color.light_blue));
         }
+    }
+
+    @OnClick(R.id.mSetting)
+    void onClickSettingButton(View view){
+        final String LOG = "popup menu";
+        //Creating the instance of PopupMenu
+        PopupMenu popup = new PopupMenu(MainActivity.this, view);
+        //Inflating the Popup using xml file
+        popup.getMenuInflater()
+                .inflate(R.menu.popup_menu, popup.getMenu());
+
+        //registering popup with OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.send){
+                    Log.i(LOG,"send");
+                    if (file!= null)
+                        Mailer.send(MainActivity.this,"your mail here","Compass","compass",file);
+                }
+                if (id == R.id.setTimer){
+                    Log.i(LOG,"set timer");
+                    showDialog();
+                }
+                return true;
+            }
+        });
+
+        popup.show(); //showing popup menu
     }
 
     @Override
@@ -83,8 +129,10 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         protected void onPreExecute() {
             super.onPreExecute();
             stringBuilder = new StringBuilder();
+            stringBuilder.append("Start!! \n");
             c = Calendar.getInstance();
-            Log.i(LOG_TAG,"start compass");
+            Log.i(LOG_TAG, "start compass");
+            mSetting.setEnabled(false);
         }
 
         @Override
@@ -94,9 +142,11 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
             Date date = c.getTime();
             SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
             float degree = CompassData.degree;
-            String degree_str = String.format("%.1f", degree);
+            String degree_str = String.format("%.2f", degree);
             String timestamp = df.format(date);
             stringBuilder.append(timestamp+"\t"+degree_str+"\n");
+                publishProgress();
+                Log.i(LOG_TAG,"sleep for "+CompassData.interval);
                Thread.sleep(CompassData.interval);
             }
             }
@@ -107,13 +157,48 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         }
 
         @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            String result = stringBuilder.toString();
+            Log.i(LOG_TAG, "update compass " + result);
+            textView.setText(result);
+            scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+        }
+
+        @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
+            stringBuilder.append("Stop!! \n");
             String result = stringBuilder.toString();
             Log.i(LOG_TAG, "stop compass " + result);
-            File file = SaveFile.save("compass.txt", result);
-            Mailer.send(MainActivity.this,"your mail here","Compass","compass",file);
+            textView.setText(result);
+            file = SaveFile.save("compass.txt", result);
+            mSetting.setEnabled(true);
+            scrollView.fullScroll(ScrollView.FOCUS_DOWN);
         }
+    }
+
+
+
+    private void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Set Timer");
+        final EditText input = new EditText(MainActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                CompassData.interval = Integer.parseInt(input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        builder.create().show();
+        input.setText(Integer.toString(CompassData.interval));
     }
 
     @Override
@@ -137,9 +222,8 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
     public void onSensorChanged(SensorEvent event) {
 
         // get the angle around the z-axis rotated
-        float degree = Math.round(event.values[0]);
+        float degree = event.values[0];
         CompassData.degree = degree;
-
     }
 
     @Override
